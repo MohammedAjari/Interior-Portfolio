c647bef2f2c68ef0d72dadcd6b419cd812c6e571
MohammedAjari <ajarimohammed0987@gmail.com>
2023-10-19 13:42:31 +0530

    issue solved 2

--- components/AboutComponent.jsx
+++ components/AboutComponent.jsx
@@ -18,13 +18,13 @@ export default function AboutComponent() {
                 </div>
                 <div className="items-center">
                     <p className="lg:px-12 lg:pb-4 py-3 px-4" style={{ fontFamily: 'Open Sans', fontSize: 'large' }}>
-                         Well-designed, beautiful spaces can have a calming effect and reduce stress levels, promoting a sense of tranquility and well-being.
+                        🔸Well-designed, beautiful spaces can have a calming effect and reduce stress levels, promoting a sense of tranquility and well-being.
                         <br/> <br/>
-                         Aesthetic rooms are more inviting and can make guests feel welcome and comfortable in your home.
+                        🔸Aesthetic rooms are more inviting and can make guests feel welcome and comfortable in your home.
                         <br/><br/>
-                         Classic and aesthetic designs tend to have a timeless quality, ensuring that your room's beauty remains relevant over the years.
+                        🔸Classic and aesthetic designs tend to have a timeless quality, ensuring that your rooms beauty remains relevant over the years.
                         <br/><br/>
-                         A colorful and aesthetically pleasing room can stimulate creativity and imagination. It's an inspiring environment for artistic pursuits.
+                        🔸A colorful and aesthetically pleasing room can stimulate creativity and imagination. Its an inspiring environment for artistic pursuits.
                         <br/>
                     </p>
                     <Link className="inline-flex items-center px-8 py-3 text-white rounded-full shadow-lg bg-gray-950 hover:bg-gray-800 hover:ring-2 lg:ml-10 hover:ring-gray-950 ring-offset-2 " href="/projects" style={{ fontFamily: 'Open Sans', fontSize: 'large' }} >Explore More <TbArrowUpRight className="w-5 h-5 ml-2" /></Link>
